ecb55ee3f69bcc2b3aa4239165fae1a9e3cb2aae
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2023-01-13 00:02:44 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import s from '../App.module.css';
 import Contacts from './Contacts';
 import Filter from './Filter';
 import Form from './Form';
 
 export default function App() {
-  const [contacts, setContacts] = useState({
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', tel: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', tel: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', tel: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', tel: '227-91-26' },
-    ],
-  });
-  const [filter, setFilter] = useState({
-    filter: '',
-  });
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || []
+  );
+  const [filter, setFilter] = useState('');
 
   const handleSubmit = object => {
     setContacts({ contacts: [...contacts, object] });
 
-    const checkContact = contacts.find(
-      contact => contact.name.toLowerCase() === object.name.toLowerCase()
-    );
+    // const checkContact = contacts.find(
+    //   contact => contact.name.toLowerCase() === object.name.toLowerCase()
+    // );
   };
 
   const handleDelete = id => {
-    setContacts(prevState => ({
-      contacts: prevState.contacts.filter(el => el.id !== id),
-    }));
+    setContacts(prevState => prevState.filter(el => el.id !== id));
   };
 
   const filterByName = e => {
@@ -41,6 +32,17 @@ export default function App() {
       contact.name.toLowerCase().includes(normalize)
     );
   };
+
+  useEffect(() => {
+    const getStorageContacts = localStorage.getItem('contacts');
+
+    if (getStorageContacts !== null) {
+      const contactsStorage = JSON.parse(getStorageContacts);
+      if (contactsStorage.length !== contacts.length) {
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      }
+    }
+  }, [contacts]);
   return (
     <div className={s.mainDiv}>
       <h1 className={s.mainTitle}>Phonebook</h1>
